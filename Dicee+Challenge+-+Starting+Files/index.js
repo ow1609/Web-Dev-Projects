76ae2346be4c2b9caa3189651612ad5343cd9762
Onuora Wright <onuora.wright@gmail.com>
2025-01-10 15:32:02 +0000

    change both dice images using random numbers

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,3 +1,5 @@
 let randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
+let randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
 
-document.querySelector("img").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector("img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector("img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
