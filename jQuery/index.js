fb03527b99723de7b9e8b78e31a95bb08e023bd9
Onuora Wright <onuora.wright@gmail.com>
2025-01-17 20:56:17 +0000

    use jquery to add animations

--- jQuery/index.js
+++ jQuery/index.js
@@ -29,9 +29,9 @@ $("a").attr("href", "https://www.google.com").text("Search");
 // below is jquery equivalent of the above js for loop method 
 // no need for for loop as it jquery adds it to all buttons
 // also less verbose in general with $ etc
-$("button").click(function() {
-    $("h1").css("color", "pink");
-})
+// $("button").click(function() {
+//     $("h1").css("color", "pink");
+// })
 
 // add event listener to detect for keystrokes inside out textbox (input html element) using jquery
 
@@ -70,3 +70,80 @@ $("h1").prepend("<button>Button prepended inside h1 element just after h1's open
 
 // .append() method to add an element inside another element just before its closing tag
 $("h1").append("<button>This button is appended inside the h1 element just before the h1's closing tag</button>")
+
+// the following line selects all our button elements and deletes them
+// $("button").remove();
+
+
+// Adding animation using jquery
+
+// .hide() animation method
+// our click event listener method will call the callback function
+// which will hide the h1 element when any button is clicked
+// $("button").click(function() {
+//     $("h1").hide();
+// })
+
+// to make the h1 show instead
+// $("button").click(function() {
+//     $("h1").show();
+// })
+
+
+// .toggle() to hide and show the h1
+// $("button").click(function() {
+//     $("h1").toggle();
+// })
+
+
+// .fadeOut() method 
+
+// $("button").click(function() {
+//     $("h1").fadeOut();
+// })
+
+
+// // .fadeIn() method
+// $("button").on("mouseover", function() {
+//     $("h1").fadeIn();
+// })
+
+// .fadeToggle() 
+
+// $("button").click(function() {
+//     $("h1").fadeToggle();
+// })
+
+
+// .slideUp() - useful for drop down menus
+
+// $("button").click(function() {
+//     $("h1").slideUp();
+// })
+
+// .slideDown() - useful for drop down menus
+
+// $("button").on("mouseover", function() {
+//     $("h1").slideDown();
+// })
+
+// .slideToggle() - useful for drop down menus
+
+// $("button").click(function() {
+//     $("h1").slideToggle();
+// })
+
+// .animate() method allows you to define some custom css that you want to gradually animate toward
+// $("button").on("click", function() {
+//     $("h1").animate({opacity: 0.5});
+// })
+
+// NB can only put css rules that take a numerical value inside the .animate() method
+// ie. cannot do $("h1").animate({color: red}); but CAN do $("h1").animate({margin: 20});
+// CAN do $("h1").animate({margin: "20%"}) becuase although it's a string, its still a numerical value as a percentage
+
+// chaining animations together
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+})
