f366464a75581610aa93e9c9fe8c403921b9a90a
Onuora Wright <onuora.wright@gmail.com>
2025-01-17 18:17:07 +0000

    change text using .text() method and .html() method using jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,6 @@
-$("h1").addClass("big-title margin-50");
+$("h1").addClass("big-title margin-50").text("Bye"); // .text() method 
+
+// .html() method means browser will read html tags and apply them
+// whereas .text() would include what is intended as an html tag and render it simply as a string
+
+$("button").html("<em>Hey<em>"); // changes innerHTML updating text to hey and making it italic
