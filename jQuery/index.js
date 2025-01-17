2b50c8096a73723f10b659df8cf74aae0d8aab47
Onuora Wright <onuora.wright@gmail.com>
2025-01-17 18:34:06 +0000

    use .attr() method to set href  attribute of anchor tag

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,3 +4,8 @@ $("h1").addClass("big-title margin-50").text("Bye"); // .text() method
 // whereas .text() would include what is intended as an html tag and render it simply as a string
 
 $("button").html("<em>Hey<em>"); // changes innerHTML updating text to hey and making it italic
+
+
+// .attr() method can be used to get and set the attributes of html elements
+// below jquery selects anchor tag element and sets its href attribute to google.com and adds content of anchor tag with text
+$("a").attr("href", "https://www.google.com").text("Search"); 
